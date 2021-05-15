c27e29596e00ac150635a6c4fe57acfc197c007a
jin_crates <94jingyu@gmail.com>
2021-05-16 00:00:09 +0900

    게시글 수정 화면 만들기

--- src/main/java/com/jincrates/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jincrates/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jincrates.book.springboot.service.posts;
 
 import com.jincrates.book.springboot.domain.posts.Posts;
 import com.jincrates.book.springboot.domain.posts.PostsRepository;
+import com.jincrates.book.springboot.web.dto.PostsListResponseDto;
 import com.jincrates.book.springboot.web.dto.PostsResponseDto;
 import com.jincrates.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jincrates.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,7 +35,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -41,8 +44,15 @@ public class PostsService {
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto((entity));
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)  // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,62 @@
+var main = {
+    init : function() {
+        var _this = this;
+        $('#btn-save').on('click', function() {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function() {
+            _this.update();
+        });
+    },
+    save : function() {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';  // 글 등록이 성공하면 메인페이지(/)로 이동합니다.
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function() {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',  // POstsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다.
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
+
+//http://localhost:8080/h2-console 실행
+// REST에서 CRUD는 다음과 같이 HTTP Method에 매핑된다.
+// 생성(Create) - POST
+// 읽기(Read) - GET
+// 수정(Update) - PUT
+// 삭제(Delete) - DELETE
