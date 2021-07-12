4c5c7172a9e600e10c9d0d6e0cba4b10d55c33b8
jin_crates <94jingyu@gmail.com>
2021-07-12 21:43:34 +0900

    구글 로그인테스트

--- src/main/java/com/jincrates/book/springboot/web/IndexController.java
+++ src/main/java/com/jincrates/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jincrates.book.springboot.web;
 
+import com.jincrates.book.springboot.config.auth.dto.SessionUser;
 import com.jincrates.book.springboot.service.posts.PostsService;
 import com.jincrates.book.springboot.web.dto.PostsResponseDto;
 import com.jincrates.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 1) Model
     //    - 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
@@ -22,6 +26,11 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
